8c13f9f7e1f18e3af8446d0f7918dc7ae3d32697
Thomas_Woltering <122703875+wart3k@users.noreply.github.com>
2024-08-09 23:59:59 +0200

    adding log library spdlog

--- service/main.cpp
+++ service/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include "config.h"
+#include "spdlog/spdlog.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    spdlog::info("Starting Service: {}, Version v{}", project_name, project_version);
+    spdlog::info("Using spdlog v{}.{}.{}", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
+    spdlog::info("That's it");
+
+    return EXIT_SUCCESS;
 }
